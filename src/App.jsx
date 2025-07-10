f3c2188470412c3715247e718c353925fadb2f46
Flatronn2 <ytdberat@gmail.com>
2025-07-11 00:12:37 +0300

    Dosya yapısını güncelledim

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import  {Routes, Route, BrowserRouter} from "react-router-dom"
+import Home from './pages/Home.jsx'
+import RegisterPage from './pages/Register.jsx'
+
+
 
 function App() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
   
-  
-}
-
-export default App
+  export default App
