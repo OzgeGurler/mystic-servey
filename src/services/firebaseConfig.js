aec540b349e0e50eeb26fbec43e19c1ae76e5456
OzgeGurler <ozgegurler14@gmail.com>
2025-07-13 20:11:36 +0300

    .env dosyası oluşturuldu

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeVMwLsO2raWMjiXNM6DPK_n2FvTgmuPM",
-  authDomain: "mystic-servey.firebaseapp.com",
-  projectId: "mystic-servey",
-  storageBucket: "mystic-servey.firebasestorage.app",
-  messagingSenderId: "653959214569",
-  appId: "1:653959214569:web:59a234e519692555388778",
-  measurementId: "G-DPFTG8M4ZR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
